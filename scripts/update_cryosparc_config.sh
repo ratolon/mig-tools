4858b1b7ff3a656aee30fd5797b72f961609029a
Ratolon <miceta@cnb.csic.es>
2026-06-30 16:59:55 +0200

    Add port explicitly

--- scripts/update_cryosparc_config.sh
+++ scripts/update_cryosparc_config.sh
@@ -9,6 +9,7 @@ fi
 CRYOSPARCW_BIN="$1"
 WORKER_HOSTNAME="$2"
 CRYOSPARC_MASTER_HOSTNAME="$3"
+CRYOSPARC_MASTER_PORT="$4"
 
 if [[ ! -x "$CRYOSPARCW_BIN" ]]; then
     echo "[ERROR] cryosparcw executable not found or not executable: $CRYOSPARCW_BIN"
@@ -19,6 +20,10 @@ CMD=("$CRYOSPARCW_BIN" connect --worker "$WORKER_HOSTNAME" --update)
 
 if [[ -n "$CRYOSPARC_MASTER_HOSTNAME" ]]; then
     CMD+=(--master "$CRYOSPARC_MASTER_HOSTNAME")
+fi
+
+if [[ -n "$CRYOSPARC_MASTER_PORT" ]]; then
+    CMD+=(--master "$CRYOSPARC_MASTER_PORT")
 fi
 
 CMD+=(--nossd )
